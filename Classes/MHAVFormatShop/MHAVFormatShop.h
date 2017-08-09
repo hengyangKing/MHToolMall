0f334fc6c7edfe50f3bf80ca7903f98460f4638a
hengyangKing <hengyangKing.yeah.net>
2017-08-09 13:45:09 +0800

    add window and farmatshop

--- Classes/MHAVFormatShop/MHAVFormatShop.h
+++ Classes/MHAVFormatShop/MHAVFormatShop.h
@@ -14,12 +14,16 @@
  单利创建
  */
 + (MHAVFormatShop *) shareFormatShop;
+
+
 /**
- *  将目标url转换成小格式工具方法
- *
- *  @param sourceURL          目标视频文件url
- *  @param progresCallback    进度block
- *  @param completionCallback 成功block
+   将目标url转换成小格式工具方法
+
+
+ @param config 设置
+ @param progress 进度
+ @param completion 成功回调
+ @param failed 失败回调
  */
 -(void)exportAVURLAssetWithConfig:(void (^)(MHAVFormatShopConfig *config))config andProgress:(void (^)(float progress))progress andCompletion:(void (^)(MHAVFormatShopConfig *config))completion andFailed:(void (^)())failed;
 @end
